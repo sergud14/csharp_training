0129a3d1818bb1cdbb3a89dc7c61b6190950d313
sergud14 <sergud@bk.ru>
2022-10-08 21:41:35 +0300

    Refactored tests

---
+++ addressbook-web-tests/GroupData.cs
@@ -0,0 +1,39 @@
+﻿namespace WebAddressbookTests
+{
+     class GroupData
+    {
+        private string name;
+        private string header="";
+        private string footer="";
+
+        public GroupData(string name)
+        {
+            this.name = name;
+        }
+
+        public GroupData(string name, string header, string footer)
+        {
+            this.name = name;
+            this.header = header;
+            this.footer = footer;
+        }
+
+        public string Name 
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+        public string Header 
+        { 
+            get { return header; }
+            set { header = value; }
+        }
+
+        public string Footer
+        { 
+            get { return footer; }
+            set { footer = value; }
+        }
+    }
+}
--- addressbook-web-tests/UnitTest1.cs
+++ addressbook-web-tests/UnitTest1.cs
@@ -1,27 +1,23 @@
 ﻿using System;
 using System.Text;
-using System.Text.RegularExpressions;
-using System.Threading;
 using NUnit.Framework;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
-using OpenQA.Selenium.Support.UI;
 
-namespace SeleniumTests
-{
+namespace WebAddressbookTests
+{ 
     [TestFixture]
-    public class UntitledTestCase
+    public class GroupCreationTests
     {
         private IWebDriver driver;
         private StringBuilder verificationErrors;
-        private string baseURL;
+        private string baseURL= @"http://localhost";
         private bool acceptNextAlert = true;
 
         [SetUp]
         public void SetupTest()
         {
             driver = new ChromeDriver();
-            baseURL = "https://www.google.com/";
             verificationErrors = new StringBuilder();
         }
 
@@ -34,84 +30,71 @@ namespace SeleniumTests
             }
             catch (Exception)
             {
-                // Ignore errors if unable to close the browser
             }
             Assert.AreEqual("", verificationErrors.ToString());
         }
 
         [Test]
-        public void TheUntitledTestCaseTest()
+        public void GroupCreationTest()
         {
-            driver.Navigate().GoToUrl("http://localhost/addressbook/");
-            driver.FindElement(By.Id("LoginForm")).Click();
-            driver.FindElement(By.Name("user")).Clear();
-            driver.FindElement(By.Name("user")).SendKeys("admin");
-            driver.FindElement(By.Id("LoginForm")).Click();
-            driver.FindElement(By.Name("pass")).Click();
-            driver.FindElement(By.Name("pass")).Clear();
-            driver.FindElement(By.Name("pass")).SendKeys("secret");
-            driver.FindElement(By.XPath("//input[@value='Login']")).Click();
-            driver.FindElement(By.LinkText("groups")).Click();
-            driver.FindElement(By.Name("new")).Click();
-            driver.FindElement(By.Name("group_name")).Click();
-            driver.FindElement(By.Name("group_name")).Clear();
-            driver.FindElement(By.Name("group_name")).SendKeys("q");
-            driver.FindElement(By.Name("group_header")).Click();
-            driver.FindElement(By.Name("group_header")).Clear();
-            driver.FindElement(By.Name("group_header")).SendKeys("q");
-            driver.FindElement(By.Name("group_footer")).Click();
-            driver.FindElement(By.Name("group_footer")).Clear();
-            driver.FindElement(By.Name("group_footer")).SendKeys("q");
-            driver.FindElement(By.Name("submit")).Click();
+            OpenHomePage();
+            Login(new AccountData("admin","secret"));
+            GoToGroupsPage();
+            InitGroupCreation();
+            GroupData groupData = new GroupData("name");
+            groupData.Header = "header";
+            groupData.Footer = "footer";
+            FillGroupForm(groupData);
+            SubmitGroupCreation();
+            ReturnToGroupsPage();
+        }
+
+        private void ReturnToGroupsPage()
+        {
             driver.FindElement(By.LinkText("group page")).Click();
             driver.FindElement(By.LinkText("Logout")).Click();
         }
-        private bool IsElementPresent(By by)
+
+        private void SubmitGroupCreation()
         {
-            try
-            {
-                driver.FindElement(by);
-                return true;
-            }
-            catch (NoSuchElementException)
-            {
-                return false;
-            }
+            driver.FindElement(By.Name("submit")).Click();
         }
 
-        private bool IsAlertPresent()
+        private void FillGroupForm(GroupData groupData)
         {
-            try
-            {
-                driver.SwitchTo().Alert();
-                return true;
-            }
-            catch (NoAlertPresentException)
-            {
-                return false;
-            }
+            driver.FindElement(By.Name("group_name")).Click();
+            driver.FindElement(By.Name("group_name")).Clear();
+            driver.FindElement(By.Name("group_name")).SendKeys(groupData.Name);
+            driver.FindElement(By.Name("group_header")).Click();
+            driver.FindElement(By.Name("group_header")).Clear();
+            driver.FindElement(By.Name("group_header")).SendKeys(groupData.Header);
+            driver.FindElement(By.Name("group_footer")).Click();
+            driver.FindElement(By.Name("group_footer")).Clear();
+            driver.FindElement(By.Name("group_footer")).SendKeys(groupData.Footer);
         }
 
-        private string CloseAlertAndGetItsText()
+        private void InitGroupCreation()
         {
-            try
-            {
-                IAlert alert = driver.SwitchTo().Alert();
-                string alertText = alert.Text;
-                if (acceptNextAlert)
-                {
-                    alert.Accept();
-                }
-                else
-                {
-                    alert.Dismiss();
-                }
-                return alertText;
-            }
-            finally
-            {
-                acceptNextAlert = true;
-            }
+            driver.FindElement(By.Name("new")).Click();
+        }
+
+        private void GoToGroupsPage()
+        {
+            driver.FindElement(By.LinkText("groups")).Click();
+        }
+
+        private void Login(AccountData account)
+        {
+            driver.FindElement(By.Name("user")).Clear();
+            driver.FindElement(By.Name("user")).SendKeys(account.Username);
+            driver.FindElement(By.Name("pass")).Clear();
+            driver.FindElement(By.Name("pass")).SendKeys(account.Password);
+            driver.FindElement(By.XPath("//input[@value='Login']")).Click();
+        }
+
+        private void OpenHomePage()
+        {
+            driver.Navigate().GoToUrl(baseURL+ "/addressbook/");
         }
     }
 }
